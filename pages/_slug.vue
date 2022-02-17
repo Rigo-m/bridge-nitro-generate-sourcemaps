6b87422a5ac3a330ae4693baeac8c5d36e030b3c
Matteo Rigoni <matteo.rigoni@atoms.studio>
2022-02-17 12:57:54 +0100

    fix: added netlify.toml

---
+++ pages/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="someclass">
+    {{ $route.params.slug }}
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.someclass {
+  transform: scale(2);
+}
+</style>
